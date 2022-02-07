5b5fc686d9561009765289759036b67bcf266432
KocoTiger <98165704+KocoTiger@users.noreply.github.com>
2022-02-07 20:19:05 +0900

    BitCampComputer01

---
+++ java_tutorials/src/실습과제04.java
@@ -0,0 +1,57 @@
+/*
+실습과제04
+
+<<<<<<<OOP의 이해/Report제출>>>>>>>>
+1. OOP(Object Oriented Programming)
+
+2. Object(객체)
+
+3. Class
+
+4. abstraction(추상화)
+
+5. Generalization(일반화)/Inheritance(상속)
+
+6. Specialization(구체적)
+
+7. Realization
+
+8. Association
+
+9. dependency
+
+10. polymorphism
+
+11. Encapsulation (정차은닉, 정보은닉)
+
+
+==>	Report 제출 condition
+==>	제출 FileName : OOPPreport.txt
+==> 편집기 : editplus
+==> font: 돋음체, fontSize : 12, 글꼴 : Reqular
+==> 문서/영구적성정/파일/설정 및 구문강조/자동줄바꿈/자동줄바꿈 check
+==> File/인쇄설정/위, 아래,왼쪽, 오른쪽 여백 모두 1.0cm
+==> A4용지 단면 2장 분량
+
+
+
+
+ */
+
+
+
+
+
+
+
+
+package java_tutorials_bitCamp1;
+
+public class 실습과제04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
