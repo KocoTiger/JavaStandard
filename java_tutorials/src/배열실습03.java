e2ebd145235277280b83a9091b01af448d1734a1
KocoTiger <98165704+KocoTiger@users.noreply.github.com>
2022-02-08 16:55:41 +0900

    Add files via upload
    2022.02.08 프린트 빼먹은 부분

---
+++ java_tutorials/src/배열실습03.java
@@ -0,0 +1,34 @@
+/*
+<<<<<<<<<<<<<<<<배열 실습>>>>>>>>>>>>>
+FileName : Example5.java
+# 문자열을 관리하는 String Array(String[][]text = new String[9][9])를 만들고,
+# 구구단 문자열을 text Array에 저장하여 출력하는 예제
+
+실행예> java Example5
+	==>실행결과(출력결과...)
+	1단을 출력합니다.
+	text[0][0]: 1*1=1
+	text[0][1]:	1*2=2
+	....
+	text[8][8]: 9*9=81
+
+	==>아래의 내용을 참조하여 만들어 보자...
+	int l = 9;
+	int j = 9;
+
+	text[8][8] = l + "*" +j+ "=" + l*j
+
+*/
+
+
+
+
+
+
+class  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
