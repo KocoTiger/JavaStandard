3541df02bf90c6a92c57a34eb132c57612e3ba7b
KocoTiger <98165704+KocoTiger@users.noreply.github.com>
2022-02-08 20:39:13 +0900

    못한문제, 2번에 3번째,배열실습03

---
+++ java_tutorials/src/BitCamp_Code_20220208/Exercise024.java
@@ -0,0 +1,56 @@
+/*
+<<<<<<<<<<<<<<<<배열 실습>>>>>>>>>>>>>
+FileName : Example5.java
+# 문자열을 관리하는 String Array(String[][]text = new String[9][9])를 만들고,
+# 구구단 문자열을 text Array에 저장하여 출력하는 예제
+
+실행예> java Example5
+	==>실행결과(출력결과...)
+	1단을 출력합니다.
+	text[0][0]: 1*1=1
+	text[0][1]:	1*2=2
+	....
+	text[8][8]: 9*9=81
+
+	==>아래의 내용을 참조하여 만들어 보자...
+	int l = 9;
+	int j = 9;
+
+	text[8][8] = l + "*" +j+ "=" + l*j
+
+*/
+
+
+//배열 실습 03... 아 어떻게 해야되지..
+class Exercise024 
+{
+	public static void main(String[] args) 
+	{
+		int [][] array = new int [9][9];
+		int l = 9;
+		int j = 9;
+		
+		
+//		int j=0;
+//
+//		while(j<9){
+//			j++;
+//			System.out.println(j+"단을 출력합니다.");
+//		
+//		//for문은 while문과는 달리 초기화를 for문 내부에서 한다.
+//		for(int i=1; i<10; i++){
+//			System.out.println(j+"*"+i+"="+j*i);
+//			}
+//		}
+
+			
+
+
+		
+		
+		
+		
+		
+		System.out.println();
+	}
+}
