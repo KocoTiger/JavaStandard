b761081e0f032157b7389277764105daf99b37c1
KocoTiger <98165704+KocoTiger@users.noreply.github.com>
2022-02-08 18:45:25 +0900

    Add files via upload
    2022.02.08

---
+++ java_tutorials/src/BitCamp_Code_20220208/BreakTest.java
@@ -0,0 +1,46 @@
+//package jb02.part03;
+/*
+	FileName: Break Test.java
+
+	//==>keyword:break ==> 자신이 속한 반복문은 종료
+	 
+	//==>break문을 이용하여 입력받은 수까지의 합 구하자
+	//==>java BreakTest10 ==> 이면 1~10더해서 55출력
+
+ */
+
+
+
+
+
+
+
+class BreakTest {
+//main method
+	public static void main(String[] args) {
+		
+		//입력받은 data를 int변환
+		int inputData=Integer.parseInt(args[0]);
+		
+		//while문의 초기화 변수 선언
+		int i=0;
+		
+		//더한값을 저장하기 위한 변수 선언
+		int sum=0;
+		
+		while(true) {
+			i++;
+			sum=sum+i;
+			//sum+=i;
+			
+			if(i==inputData) {
+				break;
+			}
+		}//end of while
+		
+		System.out.println("0~"+inputData+"까지의 합은:"+sum+"입니다.");
+		
+
+	}//end of main
+
+}//end of class
---
+++ java_tutorials/src/BitCamp_Code_20220208/실습과제04.java
@@ -0,0 +1,52 @@
+/*
+실습과제04
+
+<<<<<<<OOP의 이해/Report제출>>>>>>>>
+1. OOP(Object Oriented Programming)
+
+2. Object(객체)
+
+3. Class
+
+4. abstraction(추상화)
+
+5. Generalization(일반화)/Inheritance(상속)
+
+6. Specialization(구체적)
+
+7. Realization
+
+8. Association
+
+9. dependency
+
+10. polymorphism
+
+11. Encapsulation (정차은닉, 정보은닉)
+
+
+==>	Report 제출 condition
+==>	제출 FileName : OOPPreport.txt
+==> 편집기 : editplus
+==> font: 돋음체, fontSize : 12, 글꼴 : Reqular
+==> 문서/영구적성정/파일/설정 및 구문강조/자동줄바꿈/자동줄바꿈 check
+==> File/인쇄설정/위, 아래,왼쪽, 오른쪽 여백 모두 1.0cm
+==> A4용지 단면 2장 분량
+
+
+
+
+ */
+
+
+
+
+
+
+class  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
